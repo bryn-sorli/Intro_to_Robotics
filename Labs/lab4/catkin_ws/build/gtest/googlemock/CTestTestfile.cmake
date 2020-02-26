bf2f23d7c74a48bff177b54eadcd5a5d1d35e802
Adam Rosa <adam.rosa@colorado.edu>
2020-02-26 14:26:43 -0700

    Added catkin_ws

---
+++ Labs/lab4/catkin_ws/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/adam/CSCI 3302/Intro_to_Robotics/Labs/lab4/catkin_ws/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
